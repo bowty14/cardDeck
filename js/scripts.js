79e0cb4a5ccf68babaf15ed61961d91b4d84fbc4
Jozy Kinnaman <jozypants@gmail.com>
2020-03-30 15:59:31 -0700

    debug files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
-document.ready(function){
-  $(".btn").click(function(event){
+$(document).ready(function() {
+  $("#shuffle").click(function(event) {
     event.preventDefault();
+    var suits = ["hearts", "spades", "diamonds", "clubs"];
+    var ranks = [
+      "ace",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "Jack",
+      "Queen",
+      "King"
+    ];
+
+    suits.forEach(function(suit) {
+      ranks.forEach(function(rank) {
+        var card = (rank + " of " + suit)
+        $("#cards").append("<li>" + card + "</li>");
+      });
+    });
   });
 });
